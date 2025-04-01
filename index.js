861364fbe9505bcdc14b7dc5b91018877afdf6ed
Roman Egorov <hihihi.ru1@mail.ru>
2025-04-01 21:32:05 +0300

    main

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const titleInputNode = document.querySelector('.js-title-input');
+const  publicBtnNode= document.querySelector('.js-public-btn');
+let inputTitle = '';
+
+publicBtnNode.addEventListener('click', function () {
+    inputTitle = titleInputNode.value;
+    
+    console.log(inputTitle)
+})
+
 console.log(1);
