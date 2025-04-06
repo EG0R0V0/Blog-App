f286b104c9fd756d342a647e11fe35b1e99784bd
Roman Egorov <hihihi.ru1@mail.ru>
2025-04-06 20:18:14 +0300

    main

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
+let post = { //создаем объект 
+    title: '',
+    text: '',
+};
+
 const titleInputNode = document.querySelector('.js-title-input');
-const  publicBtnNode= document.querySelector('.js-public-btn');
-let inputTitle = '';
+const textInputtNode = document.querySelector('.js-text-input');
+const publicBtnNode = document.querySelector('.js-public-btn'); //находим элемент по классу
+const postNode = document.getElementById('js-post'); //находим элемент по id  в html
+
+
 
 publicBtnNode.addEventListener('click', function () {
-    inputTitle = titleInputNode.value;
-    
-    console.log(inputTitle)
+    const postFromUser = getPostFromUser();
+    setPost(postFromUser);
+    renderPost();
 })
 
-console.log(1);
+function getPostFromUser() {     // получить данные из поля ввода
+    const title = titleInputNode.value;
+    const text = textInputtNode.value;
+    
+    return{
+        title: title, //синтаксис позволяет псать title, если назваение переменной совпадает с ее значением
+        text: text
+    };
+}
+
+function setPost(newPost) {    // сохранить пост
+    post = newPost;
+}
+
+function getPost() {
+    return post; // если в js нужно получить какие-либо данные мы делаем это через функцию, а не напрямую
+}
+
+function renderPost() {     // отобразить пост
+    const post = getPost();
+
+    const postHTML =`
+        <div class = 'post'>
+            <p class = 'post__title'>${post.title}</p>
+            <p class = 'post__text'>${post.text}</p>
+        </div>    
+    `;
+
+    postNode.innerHTML = postHTML;
+}
